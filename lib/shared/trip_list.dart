540a48ce400e94499706780cf2cf63d2e2fd4d49
kritartha bikram shah <thisiskritartha@gmail.com>
2023-11-13 13:28:27 +0545

    Initial Commit

--- lib/shared/trip_list.dart
+++ lib/shared/trip_list.dart
@@ -11,12 +11,15 @@ class TripList extends StatefulWidget {
 
 class _TripListState extends State<TripList> {
   List<Widget> _tripTiles = [];
-  final GlobalKey _listKey = GlobalKey();
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
 
   @override
   void initState() {
     super.initState();
-    _addTrips();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _addTrips();
+    });
+    //_addTrips();
   }
 
   void _addTrips() {
@@ -31,6 +34,7 @@ class _TripListState extends State<TripList> {
 
     for (var trip in _trips) {
       _tripTiles.add(_buildTile(trip));
+      _listKey.currentState?.insertItem(_tripTiles.length - 1);
     }
   }
 
@@ -82,13 +86,18 @@ class _TripListState extends State<TripList> {
     );
   }
 
+  Tween<Offset> position = Tween(begin: Offset(1, 0), end: Offset(0, 0));
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return AnimatedList(
         key: _listKey,
-        itemCount: _tripTiles.length,
-        itemBuilder: (context, index) {
-          return _tripTiles[index];
+        initialItemCount: _tripTiles.length,
+        itemBuilder: (context, index, animation) {
+          return SlideTransition(
+            position: animation.drive(position),
+            child: _tripTiles[index],
+          );
         });
   }
 }
